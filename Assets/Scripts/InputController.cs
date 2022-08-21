79ad1f78f04f1ed04fa0300958ea1c52577701b8
Thomas <thomas.yanuziello@gmail.com>
2022-08-21 19:26:19 -0400

    Initialize

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public Player p1, p2;
+
+    public Player current;
+
+    
+}
